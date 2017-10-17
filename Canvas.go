ab69a306944e041e20449f3cada4ee7ae4d8ed8a
Alexis <Alexis.Chuah@hsutx.edu>
2017-10-17 12:00:23 -0500

    Get file names using gjson.

--- Canvas.go
+++ Canvas.go
@@ -1,34 +1,76 @@
 package main
 
 import (
+    "bufio"
+    "crypto/hmac"
+    "crypto/sha256"
+    "encoding/base64"
     "fmt"
+    "github.com/tidwall/gjson"
+    "io/ioutil"
     "log"
-    "sort"
-    "time"
-    "strings"
+    "net/http"
     "net/url"
+    "os"
+    "sort"
+    "strings"
+    "time"
 )
+
+//Define File Message Structure
+type FileMessage struct{
+    Filename    string  `json:"filename"`
+    Table       string  `json:"table"`
+    FileURL     string  `json:"url"`
+    Partial     bool    `json:"partial"`
+}
+
+//Define Response Message Structure
+type Message struct{
+    SchemaVersion   string          `json:"schemaVersion"`
+    Incomplete      bool            `json:"incomplete"`
+    Files           []FileMessage   `json:"files"`
+}
 
 //Read File for Secret and Key
 func ReadFile() (key, secret string){
-    //path := "Special.txt"
-    key = "Hello"
-    secret = "world"
+    path := "Special.txt"
+    file, err := os.Open(path)
+    if err !=nil {
+        log.Fatal(err)
+    }
+    defer file.Close()
+    var lines []string
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan(){
+        lines = append(lines, scanner.Text())
+    }
+
+    key = lines[0]
+    secret = lines[1]
     return key, secret
 }
 
-//Create Hashed Message
-func SignedMessage(address, secret string) string{
-    //timestamp := strings.Replace(time.Now().UTC().Format(time.RFC1123), "UTC", "GMT", -1)
-    //Testing purposes
-    //timestamp := time.Date(2015, time.December, 1, 9, 24, 50, 0, time.UTC)
-    u, err := url.Parse(address)
+//Generate message for hashing
+func GenerateMessage(secret, timestamp, canvasURL string) string{
+    u, err := url.Parse(canvasURL)
     if err != nil{
         log.Fatal(err)
     }
-    address = SortParams(u.RawQuery)
-    fmt.Println(address)
-    return "hello"
+    queryStr := SortParams(u.RawQuery)
+    msgSlice := []string {"GET", u.Host, "", "", u.Path, queryStr, timestamp, secret}
+    msg := strings.Join(msgSlice, "\n")
+
+    return msg
+}
+
+//HMAC hash creation
+func ComputeHash(timestamp, canvasURL, secret string) string{
+    msg := GenerateMessage(secret, timestamp, canvasURL)
+    byteSecret := []byte(secret)
+    hash := hmac.New(sha256.New, byteSecret)
+    hash.Write([]byte(msg))
+    return base64.StdEncoding.EncodeToString(hash.Sum(nil))
 }
 
 //Sort URL query parameters alphabetically
@@ -51,10 +93,41 @@ func SortParams(rawQuery string) string{
     return rawQuery
 }
 
+//Generate Headers and parse response --Current WIP--
+func httpRequest(msg, key, canvasURL, timestamp string){
+
+    client := &http.Client{}
+    req, err := http.NewRequest("GET", canvasURL, nil)
+    req.Header.Add("Authorization", "HMACAuth " + key + ":" + msg)
+    req.Header.Add("Date", timestamp)
+    resp, err := client.Do(req)
+    if err !=nil {
+        fmt.Println("ERROR", err)
+    }
+    if resp !=nil{
+        defer resp.Body.Close()
+    }
+
+    //body response returned in bytes
+    body, readErr := ioutil.ReadAll(resp.Body)
+    if readErr !=nil{
+        fmt.Println("body error:", readErr)
+    }
+
+    var respMsg Message
+    gjson.Unmarshal(body, &respMsg)
+    fmt.Printf("Schema Version: %s, Incomplete: %t \n", respMsg.SchemaVersion, respMsg.Incomplete)
+    fmt.Println("File names:")
+    for i:=0; i<5; i++{
+        fmt.Println(respMsg.Files[i].Filename)
+    }
+}
+
 func main(){
     key, secret := ReadFile()
-    address :=  "https://portal.inshosteddata.com/api/account/self/dump?limit=100&after=45"
+    canvasURL :=  "https://portal.inshosteddata.com/api/account/self/file/sync"
+    //Timestamp, replace UTC with GMT and convert to string in standard RFC1123 format
     timestamp := strings.Replace(time.Now().UTC().Format(time.RFC1123), "UTC", "GMT", -1)
-    msg := SignedMessage(address, secret)
-    fmt.Printf("Key: %s \n Secret: %s\n Time: %s\n URL: %s\n Message: %s\n", key, secret, timestamp, address, msg)
+    msg := ComputeHash(timestamp, canvasURL, secret)
+    httpRequest(msg, key, canvasURL, timestamp)
 }
